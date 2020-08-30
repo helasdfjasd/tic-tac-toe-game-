c44ab158ec434074f7687a6706c3c346d4f6f105
DevYasin <yasinalipour057@gmail.com>
2020-08-30 17:41:35 +0430

    Checked Winning Condition in total of 8 conditions.

--- app.py
+++ app.py
@@ -12,12 +12,13 @@ def print_board(board):
 # print_board(the_board)
 
 def game():
-    turn = 'x'
+    
+    turn = 'X'
     count = 0
 
     for i in range(10):
         print_board(the_board)
-        print("نوبت شماست" + turn + "به کدام قسمت میخواید حرکت کنید؟")
+        print("Your turn" + turn + ".Move to which place?")
 
         move = input()
 
@@ -25,5 +26,54 @@ def game():
             the_board[move] = turn
             count += 1
         else:
-            print("این قسمت قبلا پر است.\n به کدام قسمت میخواید حرکت کنید")
+            print("That place is already filled.\nMove to which place?")
             continue
+        # Now we will check if player X or O has won, for every move after 5 move.
+
+        if count >= 5:
+            if the_board['7'] == the_board['8'] == the_board['9'] != ' ':
+                print_board(the_board)
+                print('\n Game Over. \n')
+                print(" **** " + turn + " Won." + " **** ")
+                break
+        elif the_board['4'] == the_board['5'] == the_board['6'] != ' ':
+            print_board(the_board)
+            print('\n Game Over. \n')
+            print(" **** " + turn + " Won." + " **** ")
+            break
+        elif the_board['1'] == the_board['2'] == the_board['3'] != ' ':
+            print_board(the_board)
+            print('\n Game Over. \n')
+            print(" **** " + turn + " Won." + " **** ")
+            break
+        elif the_board['2'] == the_board['5'] == the_board['8'] != ' ':
+            print_board(the_board)
+            print('\n Game Over. \n')
+            print(" **** " + turn + " Won." + " **** ")
+            break
+        elif the_board['3'] == the_board['6'] == the_board['9'] != ' ':
+            print_board(the_board)
+            print('\n Game Over. \n')
+            print(" **** " + turn + " Won." + " **** ")
+            break
+        elif the_board['7'] == the_board['5'] == the_board['3'] != ' ':
+            print_board(the_board)
+            print('\n Game Over. \n')
+            print(" **** " + turn + " Won." + " **** ")
+            break
+        elif the_board['1'] == the_board['5'] == the_board['9'] != ' ':
+            print_board(the_board)
+            print('\n Game Over. \n')
+            print(" **** " + turn + " Won." + " **** ")
+            break
+    
+    # If neither X nor O wins and the board is full, we'll declare the result as 'Draw'.
+    if count == 9:
+        print("\nGame over.\n")
+        print("It is draw")
+
+    # We have to change the player after every move.
+    if turn == "X":
+        turn = "O"
+    else:
+        turn = "X"
